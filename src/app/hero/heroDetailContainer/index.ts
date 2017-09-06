8ef2b6fa1a716613f773e4b137196e51ed427058
Jeremy Zerr <jeremy@zerrtech.com>
2017-09-05 21:26:56 -0600

    Made the routes the same as the react app

--- src/app/hero/heroDetailContainer/index.ts
+++ src/app/hero/heroDetailContainer/index.ts
@@ -29,7 +29,7 @@ export class HeroDetailContainerComponent {
   }
 
   onClose() {
-    this.router.navigate(['']);
+    this.router.navigate(['/']);
   }
 
   ngOnDestroy() {
--- src/app/hero/routing.ts
+++ src/app/hero/routing.ts
@@ -2,7 +2,6 @@ import { HeroListContainerComponent } from './heroListContainer';
 import { HeroDetailContainerComponent } from './heroDetailContainer';
 
 export const HeroRoutes = [
-  { path: '', redirectTo: '/heroes', pathMatch: 'full' },
-  { path: 'heroes', component: HeroListContainerComponent },
+  { path: '', component: HeroListContainerComponent },
   { path: 'hero/:id', component: HeroDetailContainerComponent }
 ];
