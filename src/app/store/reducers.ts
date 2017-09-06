10063f07aa021bedb0198a309b152f5d040af2c1
Jeremy Zerr <jeremy@zerrtech.com>
2017-09-05 20:57:30 -0600

    fixed reducer formatting

--- src/app/store/reducers.ts
+++ src/app/store/reducers.ts
@@ -5,8 +5,7 @@ import { createHeroAPIReducer } from '../hero/api/reducer';
 
 // Define the global store shape by combining our application's
 // reducers together into a given structure.
-export const rootReducer =
-  combineReducers<IAppState>({
-    heroes: createHeroAPIReducer(),
-    router: routerReducer,
-  });
+export const rootReducer = combineReducers<IAppState>({
+  heroes: createHeroAPIReducer(),
+  router: routerReducer,
+});
